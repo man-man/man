d3d63d208dc43dccf7dda2fe4fa7d163ad38a18e
samoin <19909016@qq.com>
2014-02-24 15:32:05 +0800

    post方法的修改
    主要修改了公用类中的post的方法，在服务端接受的时候，需从流中读取，然后拼装到对应的paramMap中使用

---
+++ src/com/app/common/JSONArraySerializalble.java
@@ -0,0 +1,18 @@
+package com.app.common;
+
+import java.io.Serializable;
+
+import org.json.JSONArray;
+
+/**
+ * JSONArray的序列化扩展，主要是bundler不支持直接对object对象的put，只支持序列化或另一个proxxx的方法，所以这里，
+ * 进行序列化的扩展
+ * 
+ * @author samoin
+ * 
+ */
+public class JSONArraySerializalble extends JSONArray implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+}
