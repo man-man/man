5861acff6c985add4eb239bd39a18ab036b017b4
samoin <19909016@qq.com>
2014-03-13 11:12:58 +0800

    修改urlbase地址为测试服的地址

--- src/com/app/common/HttpRequestUtils.java
+++ src/com/app/common/HttpRequestUtils.java
@@ -25,7 +25,7 @@ import android.os.StrictMode;
 import com.app.man.R;
 
 public class HttpRequestUtils {
-	public static String BASE_HTTP_CONTEXT = "http://10.10.10.99:8080/manWear/";
+	public static String BASE_HTTP_CONTEXT = "http://115.28.152.215/manWear/";
 
 	public static String BUNDLE_KEY_ISPOST = "isPost";
 	public static String BUNDLE_KEY_HTTPURL = "httpUrl";
